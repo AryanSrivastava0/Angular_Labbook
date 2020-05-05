bc2ef5a8a02e265dfe8f243dd8811e171db03098
Aryan Srivastava <49358811+AryanSrivastava0@users.noreply.github.com>
2020-05-05 20:39:20 +0530

    Angular+Rest

---
+++ Lab5/src/app/app.module.ts
@@ -0,0 +1,19 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+import {AddMovieComponent} from './app.addmovie'
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+    imports: [
+        BrowserModule,FormsModule
+        
+    ],
+    declarations: [
+        AppComponent,AddMovieComponent,
+		],
+    providers: [ ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
